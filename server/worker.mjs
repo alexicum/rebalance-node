d7fdd9980fbc6e2e5cf3056d2bfd80fff05a4856
Alexey Shestakov <alshestakov25@gmail.com>
2018-10-09 02:39:48 +0300

    With error while resolving __dirname + ../react-ui/build

--- server/worker.mjs
+++ server/worker.mjs
@@ -6,19 +6,25 @@ import path from 'path';
 // eslint-disable-next-line no-unused-vars
 // import init from './init';
 import router from './router.mjs';
-import expose from './utils/expose';
 
-const { __dirname } = expose;
+// eslint-disable-next-line no-underscore-dangle
+const __dirname = path.dirname(new URL(import.meta.url).pathname);
 
 const PORT = process.env.port || 5000;
 // const REACT_UI_PATH = '../../../cra-rest-node/react-ui/build';
-const REACT_UI_PATH = '../react-ui/build';
+const REACT_UI_PATH = '../react-ui/build/';
 
 const worker = () => {
   const app = express();
-
+  // console.log('path resolve: ', path.resolve(__dirname, REACT_UI_PATH));
+  const staticUrl = new URL(REACT_UI_PATH, import.meta.url);
+  console.log('URL resolve: ', staticUrl.pathname);
+  console.log('new URL(index.html, staticUrl)', new URL('index.html', staticUrl).pathname);
+  console.log('path.resolve(__dirname, REACT_UI_PATH)', path.resolve(__dirname, REACT_UI_PATH));
   // Priority serve any static files.
-  app.use(express.static(path.resolve(__dirname, REACT_UI_PATH)));
+  // app.use(express.static(path.resolve(__dirname, REACT_UI_PATH)));
+  // app.use(express.static(new URL(REACT_UI_PATH, import.meta.url).pathname));
+  app.use(express.static(staticUrl.pathname));
 
   // // Answer API requests.
   // app.get('/api', function (req, res) {
@@ -30,7 +36,10 @@ const worker = () => {
 
   // All remaining requests return the React app, so it can handle routing.
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, REACT_UI_PATH, 'index.html'));
+    // res.sendFile(path.resolve(__dirname, REACT_UI_PATH, 'index.html'));
+    // res.sendFile(path.resolve(import.meta.url, REACT_UI_PATH, 'index.html'));
+    console.log('new URL(index.html, staticUrl).pathname', new URL('index.html', staticUrl).pathname);
+    res.sendFile(new URL('index.html', staticUrl).pathname);
   });
 
   // Error handler
