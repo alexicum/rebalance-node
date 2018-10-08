2c5924ac26767f5e8dcd3bf9c2e61148cb5e1792
Alexey Shestakov <alshestakov25@gmail.com>
2018-10-08 18:49:16 +0300

    disable cluster

--- server/index.mjs
+++ server/index.mjs
@@ -1,13 +1,13 @@
 import worker from './worker.mjs';
-import startInCluster from './utils/startInCluster.mjs';
+// import startInCluster from './utils/startInCluster.mjs';
 
 // eslint-disable-next-line no-unused-vars
 // import init from './init';
 
-const DEBUG = !(process.env.NODE_ENV === 'production');
+// const DEBUG = !(process.env.NODE_ENV === 'production');
 
-if (DEBUG) {
-  worker();
-} else {
-  startInCluster(worker);
-}
+// if (DEBUG) {
+worker();
+// } else {
+//   startInCluster(worker);
+// }
